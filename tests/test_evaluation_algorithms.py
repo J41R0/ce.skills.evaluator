69787a2ab7be6325ea88737fb94f95e834bcf271
Jairo Lefebre <yodax@aleph.engineering>
2020-06-11 14:49:04 -0400

    Add GitHubEvaluatorTests class  - Define setUp method for the test cases  - Add test_preprocess_fuction to test the GitHub preprocessor

--- tests/test_evaluation_algorithms.py
+++ tests/test_evaluation_algorithms.py
@@ -6,6 +6,7 @@ import unittest
 sys.path.append('../')
 from evaluator.app.factory import ProfileFactory
 from evaluator.domain.algorithms.default import DefaultEvaluator, DefaultPreprocessor
+from evaluator.domain.algorithms.GitHub import GitHubEvaluator, GitHubPreprocessor
 
 
 class DefaultEvaluatorTests(unittest.TestCase):
@@ -72,3 +73,19 @@ class DefaultEvaluatorTests(unittest.TestCase):
     def test_default_preprocessor(self):
         preprocessor = DefaultPreprocessor()
         self.assertEqual(self.default_profile, preprocessor.preprocess(self.default_profile))
+
+
+class GitHubEvaluatorTests(unittest.TestCase):
+    def setUp(self):
+        current_file_path = os.path.abspath(os.path.dirname(__file__))
+        default_evaluation_input_path = os.path.join(current_file_path, "resources/default_evaluation_input.json")
+        with open(default_evaluation_input_path, 'r') as file:
+            def_input = file.read()
+
+        def_input_dict = json.loads(def_input)
+        self.git_hub_profile = ProfileFactory.from_dict(def_input_dict['profiles'][0])
+        self.scale_lower_bound = def_input_dict['scale_lower_bound']
+        self.scale_higher_bound = def_input_dict['scale_higher_bound']
+
+    def test_preprocess_fuction(self):
+        pass
