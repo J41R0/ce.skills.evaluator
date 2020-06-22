7e3cedb62b2ead7446a79b2f6a313b9e40da1a4c
Jairo Lefebre <yodax@aleph.engineering>
2020-06-22 16:03:10 -0400

    Refactor load_project_fcm function due FCM library update

--- evaluator/domain/knowledge_base/__init__.py
+++ evaluator/domain/knowledge_base/__init__.py
@@ -1,7 +1,9 @@
-from Py_FCM import FuzzyCognitiveMap
+from Py_FCM import from_json, FuzzyCognitiveMap
 
 
 def load_project_fcm():
     with open('kb_stored/project_kb.json', 'r') as file:
         project_kb_json = file.read()
-        return FuzzyCognitiveMap.from_json(project_kb_json)
+        knowledge_base = from_json(project_kb_json)
+        knowledge_base.set_map_decision_function("exited")
+        return knowledge_base
