f95238bca2f8044055dda59974931316ed6e5a50
Jairo Lefebre <yodax@aleph.engineering>
2020-06-22 16:17:44 -0400

    Add evaluate implementation to GitHubEvaluator  Add GitHub evaluation process first approach

--- evaluator/domain/algorithms/GitHub.py
+++ evaluator/domain/algorithms/GitHub.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 
+from Py_FCM import join_maps
+
+from evaluator.domain import knowledge_base
 from evaluator.domain.profile_objects import Profile, EvaluatedSkill
 from evaluator.domain.provider_processor import Evaluator, Preprocessor
 
@@ -16,7 +19,23 @@ class GitHubEvaluator(Evaluator):
         Returns: List of EvaluatedSkill
 
         """
-        pass
+        evaluated_skill_list = []
+
+        projects_fcm = []
+        skills_relation = defaultdict(list)
+        for skill in profile.skills:
+            skills_relation[skill.repository_id].append(skill)
+        for repo_id in skills_relation:
+            projects_fcm.append(knowledge_base.load_project_fcm())
+            for skill in skills_relation[repo_id]:
+                projects_fcm[-1].init_concept(skill.name, skill.contribution_factor, required_presence=False)
+            projects_fcm[-1].run_inference()
+
+        final_fcm = join_maps(projects_fcm)
+        result = final_fcm.get_final_state(nodes_type='any')
+        for skill_name in result:
+            evaluated_skill_list.append(EvaluatedSkill(skill_name, result[skill_name]))
+        return evaluated_skill_list
 
 
 class GitHubPreprocessor(Preprocessor):
@@ -42,7 +61,7 @@ class GitHubPreprocessor(Preprocessor):
             total_project_bytes[skill.repository_id] += skill.value
 
         for skill in profile.skills:
-            skill.contribution_factor = (skill.value * projects_contribution[skill.repository_id]) / total_project_bytes[
-                skill.repository_id]
+            skill.contribution_factor = ((skill.value * projects_contribution[skill.repository_id]) /
+                                         total_project_bytes[skill.repository_id])
 
         return profile
