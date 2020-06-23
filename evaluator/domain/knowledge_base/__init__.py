3501c0e379adb51765ed48f45d3d936d2015a041
Jairo Lefebre <yodax@aleph.engineering>
2020-06-23 13:24:39 -0400

    Refactor load_providers_fcm function  To load the knowledge base json form working directory path

--- evaluator/domain/knowledge_base/__init__.py
+++ evaluator/domain/knowledge_base/__init__.py
@@ -2,7 +2,7 @@ from Py_FCM import from_json
 
 
 def load_providers_fcm():
-    with open('kb_stored/project_kb.json', 'r') as file:
+    with open('evaluator/domain/knowledge_base/kb_stored/project_kb.json', 'r') as file:
         project_kb_json = file.read()
         knowledge_base = from_json(project_kb_json)
         knowledge_base.set_map_decision_function("exited")
