4c968e63638b39aa8625e531f4133aa32d8c24bc
Jairo Lefebre <yodax@aleph.engineering>
2020-06-24 15:17:36 -0400

    Refactor evaluation_service.py  - Add to SkillsEvaluation the responsibility to scale output value from evaluation algorithms in with the function __scale_value  - Refactor SkillsEvaluation constructor to store scale_lower_bound and scale_higher_bound as class attributes  - Refactor add_skill_evaluation method to scale in the addition process  - Refactor evaluate_skills to pass scale_lower_bound and scale_higher_bound params to SkillsEvaluation class constructor

--- evaluator/app/evaluation_service.py
+++ evaluator/app/evaluation_service.py
@@ -4,13 +4,21 @@ from evaluator.app.factory import ProfileFactory
 
 
 class SkillsEvaluation:
-    def __init__(self):
+    def __init__(self, scale_lower_bound: float, scale_higher_bound: float):
         self.__skill_data = defaultdict(dict)
+        self.__scale_lower_bound = scale_lower_bound
+        self.__scale_higher_bound = scale_higher_bound
 
-    def add_skill_evaluation(self, skill_name, provider, evaluation):
+    def __scale_value(self, value: float) -> float:
+        max_positive_scale = self.__scale_higher_bound + abs(self.__scale_lower_bound)
+        scaled_in_one_to_zero_range = (value + 1) / 2
+        skill_evaluation_scaled = (scaled_in_one_to_zero_range * max_positive_scale) - abs(self.__scale_lower_bound)
+        return skill_evaluation_scaled
+
+    def add_skill_evaluation(self, skill_name, provider, evaluation: float):
         if len(self.__skill_data[skill_name]) == 0:
             self.__skill_data[skill_name]['scores'] = {}
-        self.__skill_data[skill_name]['scores'][provider] = evaluation
+        self.__skill_data[skill_name]['scores'][provider] = self.__scale_value(evaluation)
         self.__skill_data[skill_name]['name'] = skill_name
 
     def get_skills_data(self):
@@ -18,7 +26,7 @@ class SkillsEvaluation:
 
 
 def evaluate_skills(profiles: list, scale_lower_bound: float, scale_higher_bound: float) -> SkillsEvaluation:
-    evaluations = SkillsEvaluation()
+    evaluations = SkillsEvaluation(scale_lower_bound, scale_higher_bound)
     for profile in profiles:
         current_profile = ProfileFactory.from_dict(profile)
         evaluated_skills = current_profile.evaluate_skills(scale_lower_bound, scale_higher_bound)
