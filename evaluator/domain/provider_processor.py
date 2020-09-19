5896fbc20e0621ff42defeee90906a1926b00d0d
Jairo Lefebre <yodax@aleph.engineering>
2020-09-19 14:52:48 -0400

    Update SRC_LAMBDA_VALUE value   Use a 7 factor to get a more accurate evaluation

--- evaluator/domain/provider_processor.py
+++ evaluator/domain/provider_processor.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from py_fcm import functions
 
 MINIMUM_BYTES_OF_CODE_TO_CONSIDER_RELEVANT_PROJECT = 30000
-SRC_LAMBDA_VALUE = functions.Activation.sigmoid_hip_lambda(5 * MINIMUM_BYTES_OF_CODE_TO_CONSIDER_RELEVANT_PROJECT, 0.99)
+SRC_LAMBDA_VALUE = functions.Activation.sigmoid_hip_lambda(7 * MINIMUM_BYTES_OF_CODE_TO_CONSIDER_RELEVANT_PROJECT, 0.99)
 DEFAULT_LAMBDA_VALUE = 0.01
 
 
