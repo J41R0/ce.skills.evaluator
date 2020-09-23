332238ceab2e17d705b15d128fcaf8d41ab3f450
Jairo Lefebre <yodax@aleph.engineering>
2020-09-23 12:25:06 -0400

    Add naive_evaluate endpoint  To evaluate profiles using the default evaluator only without skills inference

--- evaluator/api/controller.py
+++ evaluator/api/controller.py
@@ -15,7 +15,7 @@ class Evaluator(Resource):
     @api_namespace.marshal_with(EvaluatedSkillsDto.skill_list)
     @api_namespace.expect(ProfilesDto.profile_list, validate=True)
     def post(self):
-        """Evaluate engineers according to profiles data
+        """Evaluate engineers according to profiles data using an smart evaluation process with skills inference
         ## Description
         Default evaluation service used by Cuban Engineer platform
         Example input data:
@@ -125,6 +125,127 @@ class Evaluator(Resource):
         evaluated_skills = evaluate_skills(profiles_data.profiles,
                                            profiles_data.scale_lower_bound,
                                            profiles_data.scale_higher_bound)
+        skills_json = EvaluatedSkillsDto.build_json_from_skills(evaluated_skills)
+        return skills_json, http.HTTPStatus.OK
+
+
+@api_namespace.route('naive_evaluate')
+class Evaluator(Resource):
+
+    @api_namespace.marshal_with(EvaluatedSkillsDto.skill_list)
+    @api_namespace.expect(ProfilesDto.profile_list, validate=True)
+    def post(self):
+        """Evaluate engineers according to profiles data
+        ## Description
+        Evaluation service used by Cuban Engineer platform
+        Example input data:
+
+        ```
+        {
+              "profiles": [
+                {
+                  "provider": "GITHUB",
+                  "stats": {
+                    "followers": "1"
+                  },
+                  "repositories": [
+                    {
+                      "id": 1,
+                      "isFork": false,
+                      "contributors": 0,
+                      "totalCommits": 17,
+                      "userCommits": 16,
+                      "forks": 0,
+                      "stars": 0,
+                      "views": 0,
+                      "userAdditions": 0,
+                      "totalAdditions": 0
+                    },
+                    {
+                      "id": 2,
+                      "isFork": false,
+                      "contributors": 0,
+                      "totalCommits": 0,
+                      "userCommits": 1,
+                      "forks": 0,
+                      "stars": 0,
+                      "views": 0,
+                      "userAdditions": 0,
+                      "totalAdditions": 0
+                    },
+                    {
+                      "id": 3,
+                      "isFork": false,
+                      "contributors": 0,
+                      "totalCommits": 0,
+                      "userCommits": 1,
+                      "forks": 0,
+                      "stars": 0,
+                      "views": 0,
+                      "userAdditions": 0,
+                      "totalAdditions": 0
+                    },
+                    {
+                      "id": 4,
+                      "isFork": true,
+                      "contributors": 0,
+                      "totalCommits": 0,
+                      "userCommits": 0,
+                      "forks": 0,
+                      "stars": 0,
+                      "views": 0,
+                      "userAdditions": 0,
+                      "totalAdditions": 0
+                    }
+                  ],
+                  "skills": [
+                    {
+                      "repositoryId": 1,
+                      "name": "Java",
+                      "value": 6329.0
+                    },
+                    {
+                      "repositoryId": 3,
+                      "name": "Java",
+                      "value": 2053.0
+                    }
+                  ]
+                },
+                {
+                  "provider": "GITLAB",
+                  "stats": {},
+                  "repositories": [
+                    {
+                      "id": 1,
+                      "isFork": false,
+                      "contributors": 0,
+                      "totalCommits": 16,
+                      "userCommits": 16,
+                      "forks": 0,
+                      "stars": 0,
+                      "views": 0,
+                      "userAdditions": 5,
+                      "totalAdditions": 15
+                    }
+                  ],
+                  "skills": [
+                    {
+                      "repositoryId": 1,
+                      "name": "Java",
+                      "value": 6544487.0
+                    }
+                  ]
+                }
+          ],
+          "scaleLowerBound": 1,
+          "scaleHigherBound": 10
+        }
+        """
+        profiles_data = ProfilesDto()
+        evaluated_skills = evaluate_skills(profiles_data.profiles,
+                                           profiles_data.scale_lower_bound,
+                                           profiles_data.scale_higher_bound,
+                                           smart_evaluation=False)
         skills_json = EvaluatedSkillsDto.build_json_from_skills(evaluated_skills)
         return skills_json, http.HTTPStatus.OK
 
