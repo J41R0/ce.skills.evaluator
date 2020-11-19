66823329f2818af84df25ff892e44c6c1c12255c
Jairo Lefebre <yodax@aleph.engineering>
2020-11-19 12:54:53 -0500

    Refactor github.py  Refactor GitHubEvaluator and GitHubPreprocessor to use the classes GitBasedEvaluator and GitBasedPreprocessor instead of implement both process from scratch

--- evaluator/domain/algorithms/github.py
+++ evaluator/domain/algorithms/github.py
@@ -1,14 +1,8 @@
-from collections import defaultdict
-
-from py_fcm import TYPE_SIMPLE, join_maps, functions
-
-from evaluator.domain import knowledge_base
-from evaluator.domain.profile_objects import Profile, EvaluatedSkill
-from evaluator.domain.provider_processor import Evaluator, Preprocessor
-from evaluator.domain.provider_processor import SRC_LAMBDA_VALUE
+from evaluator.domain.profile_objects import Profile
+from evaluator.domain.algorithms.git_based import GitBasedEvaluator, GitBasedPreprocessor
 
 
-class GitHubEvaluator(Evaluator):
+class GitHubEvaluator(GitBasedEvaluator):
     def evaluate(self, profile: Profile, infer_skills=True) -> list:
         """
         Evaluate a GitHub profile
@@ -19,37 +13,10 @@ class GitHubEvaluator(Evaluator):
         Returns: List of EvaluatedSkill
 
         """
-        evaluated_skill_list = []
-
-        projects_fcm = []
-        skills_relation = defaultdict(list)
-        total_project_bytes = defaultdict(int)
-        for skill in profile.skills:
-            skills_relation[skill.repository_id].append(skill)
-            total_project_bytes[skill.repository_id] += skill.value
-
-        for repo_id in skills_relation:
-            projects_fcm.append(knowledge_base.load_providers_fcm())
-            for skill in skills_relation[repo_id]:
-                projects_fcm[-1].add_concept(skill.name)
-                skill_value = skill.contribution_factor * total_project_bytes[skill.repository_id]
-                scaled_skill_value = functions.Activation.sigmoid_hip(skill_value, SRC_LAMBDA_VALUE)
-                projects_fcm[-1].init_concept(skill.name, scaled_skill_value, required_presence=False)
-            projects_fcm[-1].run_inference()
-
-        if len(projects_fcm) > 0:
-            final_fcm = join_maps(projects_fcm, ignore_zeros=True, value_strategy='highest')
-            if infer_skills:
-                result = final_fcm.get_final_state(concepts_type='any')
-            else:
-                result = final_fcm.get_final_state(concepts_type=TYPE_SIMPLE)
-            for skill_name in result:
-                if result[skill_name] > 0:
-                    evaluated_skill_list.append(EvaluatedSkill(skill_name, result[skill_name]))
-        return evaluated_skill_list
+        return self.git_evaluate(profile, infer_skills)
 
 
-class GitHubPreprocessor(Preprocessor):
+class GitHubPreprocessor(GitBasedPreprocessor):
 
     def preprocess(self, profile: Profile) -> Profile:
         """
@@ -67,15 +34,4 @@ class GitHubPreprocessor(Preprocessor):
             else:
                 projects_contribution[repository.id] = 1
 
-        total_project_bytes = defaultdict(int)
-        for skill in profile.skills:
-            total_project_bytes[skill.repository_id] += skill.value
-
-        for skill in profile.skills:
-            if total_project_bytes[skill.repository_id] > 0:
-                skill.contribution_factor = ((skill.value * projects_contribution[skill.repository_id]) /
-                                             total_project_bytes[skill.repository_id])
-            else:
-                skill.contribution_factor = 1
-
-        return profile
+        return self.git_preprocess(profile, projects_contribution)
