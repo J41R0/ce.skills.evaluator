3598feda5b9185bb6bed976824bfcba49ac6f7e4
Jairo Lefebre <yodax@aleph.engineering>
2020-12-25 18:23:21 -0500

    Refactor evaluation_service.py   Add 'final_evaluation' field to evaluate_providers_skills function output and estimate associated value   Add add_skill_final_evaluation to SkillsEvaluation class to define the final_evaluation value

--- evaluator/app/evaluation_service.py
+++ evaluator/app/evaluation_service.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 
+from py_fcm import join_maps, TYPE_SIMPLE
+
 from evaluator.app.factory import ProfileFactory
+from evaluator.domain import knowledge_base
 
 
 class SkillsEvaluation:
@@ -34,11 +37,14 @@ class SkillsEvaluation:
         skill_evaluation_scaled = self.__scale_lower_bound + pseudo_scaled_value
         return skill_evaluation_scaled
 
-    def add_skill_evaluation(self, skill_name, provider, evaluation: float):
+    def add_skill_evaluation(self, skill_name: str, provider: str, evaluation: float):
         if len(self.__skill_data[skill_name]) == 0:
             self.__skill_data[skill_name]['scores'] = {}
         self.__skill_data[skill_name]['scores'][provider] = self.__scale_value(evaluation)
         self.__skill_data[skill_name]['name'] = skill_name
+
+    def add_skill_final_evaluation(self, skill_name: str, evaluation: float):
+        self.__skill_data[skill_name]['final_evaluation'] = self.__scale_value(evaluation)
 
     def get_skills_data(self):
         return self.__skill_data.copy()
@@ -47,9 +53,25 @@ class SkillsEvaluation:
 def evaluate_providers_skills(profiles: list, scale_lower_bound: float, scale_higher_bound: float,
                               infer_skills=True, custom_evaluation=True) -> SkillsEvaluation:
     evaluations = SkillsEvaluation(scale_lower_bound, scale_higher_bound)
+    evaluations_fcm_list = []
     for profile in profiles:
         current_profile = ProfileFactory.from_dict(profile, custom_evaluation)
         evaluated_skills = current_profile.evaluate_skills(infer_skills)
+        evaluations_fcm_list.append(knowledge_base.load_skills_fcm())
         for skill in evaluated_skills:
             evaluations.add_skill_evaluation(skill.name, current_profile.provider_name, skill.value)
+            evaluations_fcm_list[-1].add_concept(skill.name)
+            evaluations_fcm_list[-1].init_concept(skill.name, skill.value)
+        evaluations_fcm_list[-1].run_inference()
+
+    final_evaluator_fcm = join_maps(evaluations_fcm_list, ignore_zeros=True, value_strategy='highest')
+    final_evaluator_fcm.set_map_decision_function("EXITED")
+    final_evaluator_fcm.run_inference()
+    if infer_skills:
+        final_evaluation = final_evaluator_fcm.get_final_state(concepts_type='any')
+    else:
+        final_evaluation = final_evaluator_fcm.get_final_state(concepts_type=TYPE_SIMPLE)
+    for skill_name in final_evaluation:
+        evaluations.add_skill_final_evaluation(skill_name, final_evaluation[skill_name])
+
     return evaluations
